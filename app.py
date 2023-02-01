626f7f34e141d007bb36f07dd19b33030206de10
Brandon Olynyk <brandontab2004@gmail.com>
2023-02-01 13:41:11 -0500

    addition and subtraction

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import discord
 import dotenv
 import os
+import requests
 dotenv.load_dotenv()
 
 
@@ -13,5 +14,17 @@ async def on_ready():
 @bot.slash_command(name = "hello", description = "Say hello to the bot")
 async def hello(ctx):
     await ctx.respond("Hey!")
+
+math = discord.SlashCommandGroup("math", "Math related commands")
+
+@math.command(name = "add", description =  "Add two numbers together")
+async def mathadd(ctx, num1: int, num2: int):
+    await ctx.respond(f"The answer is {num1+num2}!")
+
+@math.command(name = "subtract", description = "Subtract two numbers together")
+async def mathsub(ctx, num1: int, num2: int):
+    await ctx.respond(f"The answer is {num1-num2}")
+
+bot.add_application_command(math)
 
 bot.run(os.getenv("BOT_TOKEN"))
