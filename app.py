2a29a6aff341af9c137e8f180417071bf2ff0d61
Brandon Olynyk <brandontab2004@gmail.com>
2023-02-01 13:57:01 -0500

    finished math commands

--- app.py
+++ app.py
@@ -23,8 +23,18 @@ async def mathadd(ctx, num1: int, num2: int):
 
 @math.command(name = "subtract", description = "Subtract two numbers together")
 async def mathsub(ctx, num1: int, num2: int):
-    await ctx.respond(f"The answer is {num1-num2}")
+    await ctx.respond(f"The answer is {num1-num2}!")
 
+@math.command(name = "multiply", description = "Multiply two numbers together")
+async def mathmult(ctx, num1: int, num2: int):
+    await ctx.respond(f"The answer is {num1*num2}!")
+
+@math.command(name = "divide", description = "Divides two numbers together")
+async def mathdiv(ctx, numerator: int, denominator: int):
+    try:
+        await ctx.respond(f"The answer is {numerator/denominator}!")
+    except ZeroDivisionError:
+        await ctx.respond("Sorry, you can't divide by 0.")
 bot.add_application_command(math)
 
 bot.run(os.getenv("BOT_TOKEN"))
