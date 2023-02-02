cca82b5efc57e1ab92d43253203eadb8f04287c6
Brandon <brandontab2004@gmail.com>
2023-02-02 18:28:20 -0500

    progress

--- mathops.py
+++ mathops.py
@@ -1,13 +1,16 @@
-def __init__(self,num1,num2,op_char):
-    response_str = f"The answer to {num1}{op_char}{num2} is:"
-    if op_char == '+':
-	    return answer + num1+num2
-	else if op_char == '-':
-		return answer + num1-num2
-	else if op_char == '*':
-		return answer + num1*num2
-    else if op_char == '/' or op_char == '\\':
-	    if num2 != 0:
-			return answer + num1/num2
-		return "Sorry, you can't divide by 0."
-	#todo: math powers
+import math
+def do_math(self,num1,num2,op_char):
+    #todo: force op_char to a 1 length str
+	response_str = f"The answer to {num1}{op_char}{num2} is:"
+	if op_char == '+':
+		return response_str + (num1+num2)
+	elif op_char == '-':
+		return response_str + (num1-num2)
+	elif op_char == '*':
+		return response_str + (num1*num2)
+	elif op_char == '/' or op_char == "\\":
+		if num2 != 0:
+			return response_str + (num1/num2)
+	elif op_char == '^':
+		return response_str + (math.pow(num1,num2))
+	return f"{op_char} wasn't recognized as an operation! Please enter any of the following: `-`,`+`,`*`,`\\`,`/`,`^`"
