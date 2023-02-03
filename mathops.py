3c7ecaac6452b362602772b43aeadb25aad99d51
RaineXael <brandontab2004@gmail.com>
2023-02-02 19:44:07 -0500

    var type bug fixed

--- mathops.py
+++ mathops.py
@@ -1,16 +1,18 @@
 import math
-def do_math(self,num1,num2,op_char):
-    #todo: force op_char to a 1 length str
-	response_str = f"The answer to {num1}{op_char}{num2} is:"
-	if op_char == '+':
-		return response_str + (num1+num2)
-	elif op_char == '-':
-		return response_str + (num1-num2)
-	elif op_char == '*':
-		return response_str + (num1*num2)
-	elif op_char == '/' or op_char == "\\":
-		if num2 != 0:
-			return response_str + (num1/num2)
-	elif op_char == '^':
-		return response_str + (math.pow(num1,num2))
-	return f"{op_char} wasn't recognized as an operation! Please enter any of the following: `-`,`+`,`*`,`\\`,`/`,`^`"
+
+
+def do_math(num1, op_char, num2):
+    
+    response_str = f"The answer to {num1}{op_char}{num2} is:"
+    if op_char == '+':
+        return response_str + str(num1+num2)
+    elif op_char == '-':
+        return response_str + str(num1-num2)
+    elif op_char == '*':
+        return response_str + str(num1*num2)
+    elif op_char == '/' or op_char == "\\":
+        if num2 != 0:
+            return response_str + str(num1/num2)
+    elif op_char == '^':
+        return response_str + str(math.pow(num1, num2))
+    return f"{op_char} wasn't recognized as an operation! Please enter any of the following: `-`,`+`,`*`,`\\`,`/`,`^`"
