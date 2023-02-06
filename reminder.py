5b542cc5a9aef31a016ade66c9c77e7ce508cc66
RaineXael <brandontab2004@gmail.com>
2023-02-06 11:22:49 -0500

    message decryption from database

--- reminder.py
+++ reminder.py
@@ -1,6 +1,10 @@
 from cryptography.fernet import Fernet
 import aiosqlite
 from datetime import datetime
+
+#the messages here are encrypted so i don't see people's private messages
+#when doing things in the database
+
 class Reminder():
     
     def __init__(self, db_file):
@@ -15,8 +19,7 @@ class Reminder():
     async def insert_reminder(self, table_name, uid,message):
                 #returns true or false wether success or fail
                 try:
-                    db = await aiosqlite.connect(self.db_file)
-                    
+                    db = await aiosqlite.connect(self.db_file)                    
                     await db.execute(f"INSERT INTO {table_name} VALUES(?,?,?)",(uid,self.fernet.encrypt(bytes(message,'UTF-8')),str(datetime.today().strftime('%Y-%m-%d'))))
                     await db.commit()
                     await db.close()
@@ -32,12 +35,15 @@ class Reminder():
         
     async def decrypt_message(self,uid):
         db = await aiosqlite.connect(self.db_file)
-                    
-        message = await db.select('reminders','message')
-        self.fernet.decrypt(bytes(message,'UTF-8'))
+        #SELECT message FROM 'reminders' WHERE uid IS '267687811375693825'
+        cursor = await db.execute(f'SELECT message FROM \'reminders\' WHERE uid IS \'{uid}\';')
+        result = await cursor.fetchall()
+        bytestr = result[0][0]
+        truestr = self.fernet.decrypt(bytestr).decode("utf-8") 
+        await cursor.close()
         await db.commit()
         await db.close()
-        return message
+        return truestr
     
     
     
