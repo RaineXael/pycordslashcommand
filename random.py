6eb7b1aecc6a67e46cf5e99a4ad3bf62f2076422
Brandon Olynyk <brandontab2004@gmail.com>
2023-02-13 13:36:22 -0500

    skeleton random methods in random class

---
+++ random.py
@@ -0,0 +1,20 @@
+import random
+import discord
+from datetime import datetime
+class RandomNumbers:
+    def reroll_seed(self):
+        print("reroll seed based on current time")
+
+    def coin_flip(self):
+        #make an embed with the result of either 0 or 1 and make it look good
+        return ???
+
+    def d6_die(self):
+        #embed for a 6 sided die result
+        return ????
+
+    def choice_chance(self,maximum)
+        #do a roll from 0 to maximum and return an embed with the result
+        return ????
+
+    
