f81e49de91db9fa9ac35d8a348a6f6319b588721
Brandon Olynyk <brandontab2004@gmail.com>
2023-05-27 20:15:42 -0400

    removed junk and reformatted reminder.py

--- reminder.py
+++ reminder.py
@@ -3,14 +3,15 @@ from datetime import datetime
 from discord.ext import tasks, commands
 from cryptography.fernet import Fernet
 
+
 class Reminder(commands.Cog):
 
     def __init__(self, db_file, key, bot):
-            self.db_file = db_file
-            self.bot = bot
-            self.printer.start()
-            self.fernet = Fernet(bytes(key, 'utf-8'))
-            self.debug = False #set this to true to remove the negative time restricion
+        self.db_file = db_file
+        self.bot = bot
+        self.printer.start()
+        self.fernet = Fernet(bytes(key, 'utf-8'))
+        self.debug = False  # set this to true to remove the negative time restricion
 
     def cog_unload(self):
         self.printer.cancel()
@@ -23,7 +24,6 @@ class Reminder(commands.Cog):
             await cursor.close()
             await db.commit()
             await db.close()
-            print(type(result))
 
             # if > 1 entry in result return the result to be processed in remindercheck
             if len(result) > 0:
@@ -35,9 +35,8 @@ class Reminder(commands.Cog):
         except Exception as e:
             return f"An error occoured when retrieving your data: {e}"
 
-
     async def on_check_messages(self):
-        #this is to check what messages are due to send, not the /reminder check command
+        # this is to check what messages are due to send, not the /reminder check command
         try:
             db = await aiosqlite.connect(self.db_file)
             cursor = await db.execute(f'SELECT * FROM \'reminders\' WHERE datetime(time) < datetime(\'{str(datetime.now())}\');')
@@ -59,16 +58,16 @@ class Reminder(commands.Cog):
         db = await aiosqlite.connect(self.db_file)
         try:
             for reminder in result:
-                    user = await self.bot.fetch_user(int(reminder[0]))
-                    await user.send(f"<@{str(reminder[0])}>, Your reminder due for {reminder[2]}:\n\"{self.fernet.decrypt(reminder[1]).decode('utf-8')}\"")
-                    # also erase entry from db                    
-                    await db.execute(f"DELETE FROM reminders WHERE uid = ? AND message = ? and time = ?", (reminder[0], reminder[1], reminder[2]))
-                    await db.commit()                    
+                user = await self.bot.fetch_user(int(reminder[0]))
+                await user.send(f"<@{str(reminder[0])}>, Your reminder due for {reminder[2]}:\n\"{self.fernet.decrypt(reminder[1]).decode('utf-8')}\"")
+                # also erase entry from db
+                await db.execute(f"DELETE FROM reminders WHERE uid = ? AND message = ? and time = ?", (reminder[0], reminder[1], reminder[2]))
+                await db.commit()
         except Exception as e:
-            print(f"Exception occoured during removal of fulfilled request: {e}")
+            print(
+                f"Exception occoured during removal of fulfilled request: {e}")
         finally:
-            await db.close()        
-            
+            await db.close()
 
     @tasks.loop(seconds=15.0)
     async def printer(self):
@@ -105,17 +104,9 @@ class Reminder(commands.Cog):
         # check inputs then do the below
         try:
             time_obj = self.validate_time(year, month, day, hour, minute)
-            await self.insert_reminder('reminders', uid, self.fernet.encrypt(bytes(message,"utf-8")), time_obj)
+            await self.insert_reminder('reminders', uid, self.fernet.encrypt(bytes(message, "utf-8")), time_obj)
             return f'Reminder: \"{message}\" added for {time_obj}'
         except ValueError:
             return "Invalid format! Please check your numbers."
         except IndexError:
             return 'The time you entered was in the past. Please enter a future time and date.'
-
-
-# f = Fernet(key)
-
-# token = f.encrypt(b"Don't look tbh")
-# print(token)
-
-# print(str(f.decrypt(token).decode('utf-8')))
